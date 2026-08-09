10daf1aaf7ea7e87d353160fb4d81f9e1de3e41a
Sashidhar077 <sahasra972@gmail.com>
2026-08-09 03:46:33 +0000

    Module 11: Add dictionary set exercise

---
+++ 11-Dictionaries/exercise03_dict_set.tcl
@@ -0,0 +1,61 @@
+if {0} {
+Create a Tcl dictionary named employee with:
+
+name       → Ravi
+id         → 101
+age        → 22
+department → VLSI
+
+Then use dict set to:
+
+Add a new key salary with value 50000
+Add a new key experience with value 2
+Update department from VLSI to ASIC
+Print the final dictionary.
+}
+
+
+set employee [dict create \
+name        Ravi \
+id          101 \
+age         22 \
+department  VLSI \
+]
+
+puts "=============Employee dictionary==================="
+dict set $employee salary 50000
+dict set $employee experience 2
+dict set $employee department ASIC
+puts $employee
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
