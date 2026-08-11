6ce52723aaa0b5a7cdbeca3095ba362646c554c8
Sashidhar077 <sahasra972@gmail.com>
2026-08-11 03:49:16 +0000

    Module 13: Add custom package loading exercise

--- 13-Packages-and-Libraries/exercise05_loading_custom_package.tcl
+++ 13-Packages-and-Libraries/exercise05_loading_custom_package.tcl
@@ -1,3 +1,14 @@
+if {0} {
+Use the Calculator package you created in Exercise 4.
+
+Create a separate Tcl program that:
+
+Adds the package directory to auto_path.
+Loads the Calculator package using package require.
+Uses Calculator::add to add 25 and 15.
+Uses Calculator::multiply to multiply 5 and 6.
+Prints the results.
+} 
 
 lappend auto_path [pwd]
 package require Calculator 2.0
