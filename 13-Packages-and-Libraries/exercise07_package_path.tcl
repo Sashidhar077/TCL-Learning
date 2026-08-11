8987b83cddd8d97aca7cf00ce436cf360de7975e
Sashidhar077 <sahasra972@gmail.com>
2026-08-11 04:00:27 +0000

    Module 13: Add package search path exercise

---
+++ 13-Packages-and-Libraries/exercise07_package_path.tcl
@@ -0,0 +1,50 @@
+if {0} {
+Create a simple package called Greeting and load it from another Tcl script using Tcl's package search path.
+
+Your task:
+
+Create Greeting.tcl.
+Create a namespace called Greeting.
+Create a procedure hello.
+Provide the package as version 1.0.
+Create a separate application script.
+Add the package directory to auto_path.
+Use package require Greeting 1.0.
+Call Greeting::hello.
+Expected Output
+Hello from Greeting package
+}
+
+# Get the directory containing this script
+
+set package_dir [file dirname [info script]]
+
+# Add package directory to auto_path
+
+lappend auto_path $package_dir
+
+# Load Greeting package
+
+package require greet 1.0
+
+# Use the package
+
+greet::hello
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
