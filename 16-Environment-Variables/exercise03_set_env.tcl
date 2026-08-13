5b517febe06c1de2858d7d02d8c6bbe57e228719
Sashidhar077 <sahasra972@gmail.com>
2026-08-13 06:21:12 +0000

    Module 16: Add environment variable setting exercise

---
+++ 16-Environment-Variables/exercise03_set_env.tcl
@@ -0,0 +1,16 @@
+if {0} {
+Write a Tcl program that:
+
+Creates an environment variable called PROJECT_NAME.
+Assigns it the value TCL_LEARNING.
+Prints the value of the environment variable
+}
+
+set env(PROJECT_NAME) "TCL_LEARNING"
+
+puts "Project Name: $env(PROJECT_NAME)"
+
+
+
+
+
