af7c5920fe751434956060865a2dac51951b6ac9
Sashidhar077 <sahasra972@gmail.com>
2026-08-13 03:36:47 +0000

    Module 15: Add clock seconds exercise

---
+++ 15-Clock-and-Time/exercise01_clock_seconds.tcl
@@ -0,0 +1,30 @@
+if {0} {
+Write a Tcl program that:
+
+Gets the current time using clock seconds.
+Stores it in a variable called current_time.
+Prints the timestamp.
+}
+
+set current_time [clock seconds]
+
+puts "Current Unix timestamp : $current_time"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
