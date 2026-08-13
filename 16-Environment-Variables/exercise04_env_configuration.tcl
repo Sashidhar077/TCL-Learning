5f32b176589a2a2521bf93a0a6f87144c3c8f4ac
Sashidhar077 <sahasra972@gmail.com>
2026-08-13 06:23:26 +0000

    Module 16: Add environment-based configuration exercise

---
+++ 16-Environment-Variables/exercise04_env_configuration.tcl
@@ -0,0 +1,37 @@
+if {0} {
+Write a Tcl program that:
+
+Checks whether an environment variable called PROJECT_NAME exists.
+If it exists, use its value as the project name.
+If it does not exist, use "DEFAULT_PROJECT" as the project name.
+Print the selected project name.
+}
+
+if {[info exists env(PROJECT_NAME)]} {
+
+    set project_name $env(PROJECT_NAME)
+
+} else {
+
+    set project_name "DEFAULT_PROJECT"
+}
+
+puts "Project Name: $project_name"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
