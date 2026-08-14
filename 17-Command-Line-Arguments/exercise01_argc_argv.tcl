59bb4b677d340c9f69b65484cda6a136691a8248
Sashidhar077 <sahasra972@gmail.com>
2026-08-14 03:12:39 +0000

    Module 17: Add argc and argv basics exercise

---
+++ 17-Command-Line-Arguments/exercise01_argc_argv.tcl
@@ -0,0 +1,28 @@
+if {0} {
+Write a Tcl program that:
+
+Prints the number of command-line arguments.
+Prints all the arguments.
+
+Run:
+
+tclsh exercise01_argc_argv.tcl CPU_TOP ASIC
+}
+
+
+puts "Number of arguements : $argc"
+puts "\n Arguements : $argv \n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
