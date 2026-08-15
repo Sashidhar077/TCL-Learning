5818947895f96cc540f3c56a06e0cdf89bbd4c4e
Sashidhar077 <sahasra972@gmail.com>
2026-08-15 03:56:07 +0000

    Add TCL basic exec command example

---
+++ 18-System_Commands/ex1_basic_exec.tcl
@@ -0,0 +1,16 @@
+if {0} {
+Try writing a TCL program that executes these three commands:
+
+#pwd
+whoami
+date
+}
+
+puts "present working directory is  : [exec pwd]"
+
+puts "User : [exec whoami]"
+
+puts "Date : [exec date]"
+
+
+
